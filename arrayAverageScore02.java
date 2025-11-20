a4b35e432016900520dad107972d0268a8985bf8
Driann26 <adriang2610@gmail.com>
2025-11-20 15:26:09 +0700

    Question E3 No 2

--- arrayAverageScore02.java
+++ arrayAverageScore02.java
@@ -4,20 +4,35 @@ public class arrayAverageScore02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] score = new int[10];
+        System.out.print("Enter the number of students: ");
+        int n = sc.nextInt();
 
-        int passCount = 0;  
+        int[] score = new int[n];
 
-        for (int i = 0; i < score.length; i++) {
-            System.out.print("Enter student score " + (i + 1) + ": ");
+        int passCount = 0;
+        int failCount = 0;
+
+        double passTotal = 0;
+        double failTotal = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the final score " + i + ": ");
             score[i] = sc.nextInt();
 
             if (score[i] > 70) {
-                passCount++;  
+                passCount++;
+                passTotal += score[i];
+            } else {
+                failCount++;
+                failTotal += score[i];
             }
         }
 
-        System.out.println("Number of students who passed: " + passCount);
+        double passAverage = passTotal / passCount;
+        double failAverage = failTotal / failCount;
+
+        System.out.println("The average score of students who passed is " + passAverage);
+        System.out.println("The average score of students who failed is " + failAverage);
 
         sc.close();
     }
