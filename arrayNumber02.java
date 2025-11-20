1242d9c625ba42b8e7de40493ea01db8ccd47c53
Driann26 <adriang2610@gmail.com>
2025-11-20 15:02:37 +0700

    Question E1 No 3

--- arrayNumber02.java
+++ arrayNumber02.java
@@ -3,11 +3,9 @@ public class arrayNumber02 {
     public static void main(String[] args) {
         
         int[] num = {5, 12, 7, 20};
-
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
+        for (int i = 0; i < 4; i++) {
+        System.out.println(num[i]);
+        }
 
     }
 }
